2b07e2261b5f9316b28ae8dcd8391ba6357f4b1a
ryougifujino <ryougifujino@gmail.com>
2021-06-02 02:13:20 +0800

    feat(markdown-editor): simplest implementation

---
+++ markdown-editor/src/Editor.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+export let onInput: (value: string) => void
+
+const handleInput = e => onInput(e.target.value)
+</script>
+
+<textarea class="editor" on:input={handleInput}>
+</textarea>
+
+<style>
+.editor {
+  flex: 1;
+  height: 100%;
+  resize: none;
+  outline: none;
+  box-sizing: border-box;
+  border: none;
+  border-right: 1px solid gray;
+}
+</style>
---
+++ markdown-editor/src/Previewer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import marked from 'marked'
+import DOMPurify from 'dompurify'
+
+export let source: string = ''
+
+$: compiledSecureHTML = DOMPurify.sanitize(marked(source))
+</script>
+
+<article class="previewer">
+  {@html compiledSecureHTML}
+</article>
+
+<style>
+.previewer {
+  flex: 1;
+  height: 100%;
+  overflow: auto;
+  word-break: break-word;
+}
+</style>
